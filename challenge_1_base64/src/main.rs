745819629e8060793f22536ecd22d6b40d537d3e
MajorX234 <majorx234@googlemail.com>
2025-04-03 00:58:00 +0200

    Decode hex string to base64

--- challenge_1_base64/src/main.rs
+++ challenge_1_base64/src/main.rs
@@ -1,11 +1,15 @@
+use base64::prelude::*;
 use file_handler::read_arg_file;
+use hex;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() {
     let reader = read_arg_file().unwrap();
     for line in reader.lines() {
         if let Ok(line) = line {
-            println!("{}", line);
+            let decoded = hex::decode(&line).expect("Decoding failed");
+            let base64_line = BASE64_STANDARD.encode(&decoded);
+            println!("{} {}", line, base64_line);
         }
     }
 }
